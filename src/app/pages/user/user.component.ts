35d86f060cce434a5dc5c1023e212b2f1e56cc91
Zonxgh <pakawat.zon@gmail.com>
2019-08-16 13:49:39 +0700

    add ng-smart-table

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,20 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { LocalDataSource } from 'ng2-smart-table';
+import { SmartTableData } from '../../@core/data/smart-table';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      us_id: {
+        title: 'ID',
+        type: 'number',
+      },
+      us_code: {
+        title: 'Code',
+        type: 'string',
+      },
+      us_role_id: {
+        title: 'Role',
+        type: 'number',
+      },
+      us_username: {
+        title: 'Username',
+        type: 'string',
+      },
+      us_password: {
+        title: 'Password',
+        type: 'string',
+      },
+    },
+  };
+
+
   users : any;
   name : string;
-  constructor(private http: HttpClient) {}
+  source: LocalDataSource = this.users;
 
+  constructor(private http: HttpClient,private service: SmartTableData) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
   ngOnInit() {
     this.get_all_user();
   }
 
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+  
   get_all_user(){
     var db = environment.db;
     this.http.get<any>(`${db}/api/user/get_all_user`).subscribe(result => {
@@ -24,6 +80,12 @@ export class UserComponent implements OnInit {
 
   get_name(){
     this.name = 'test';
+  }
+
+  password_decode(hash){
+    const bcrypt = require('bcryptjs');
+    const status = bcrypt.compareSync(this.name,hash);
+    alert(status);
   }
 
 }
