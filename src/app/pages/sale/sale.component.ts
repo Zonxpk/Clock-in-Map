217d634795669d16c945039b88d9040b64f56f84
Zonxgh <pakawat.zon@gmail.com>
2019-08-19 16:47:12 +0700

    add category router

--- src/app/pages/sale/sale.component.ts
+++ src/app/pages/sale/sale.component.ts
@@ -1,31 +1,59 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router,ParamMap  } from '@angular/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sale',
   templateUrl: './sale.component.html',
   styleUrls: ['./sale.component.scss']
 })
-export class SaleComponent implements OnInit {
+export class SaleComponent implements OnInit , OnDestroy  {
   da: any;
   product: any;
   data: any;
   sort: any = '';
   page: any;
+  cate_id : any;
   config: any;
   count : any = 0;
   per_page: number = 6;
   collection = [];
+  token: any;
+  name: any;
+  sub: any;
+  // categorySuSubscription: Subscription;
+
 
   constructor(private http: HttpClient,private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      localStorage.clear();
+      localStorage.setItem('filter', '3');
+    }, 300000); // 5 min
 
+    this.route.paramMap.subscribe(params => {
+      this.cate_id = [params.get('categoryId')][0];
+      console.log('before cate_id',this.cate_id);
+      this.get_all();
+
+    });
+
+
+    // this.sub = this.route.params
+    // .subscribe(params => {
+    //    // get id from params
+    //    let categoryId = +params['categoryId'];
+    //     this.cate_id = categoryId;
+    //     console.log('cate_id',this.cate_id);
+    //     this.get_all();
+    //    // do whatever you want with id here
+    //  });
+   
     this.get_count().then(value => {
-      console.log('get counted',value);
       this.count = value;
       this.get_all();
     });
@@ -39,13 +67,26 @@ export class SaleComponent implements OnInit {
     .map(params => params.get('page'))
     .subscribe(page => this.config.currentPage = page);
 
+    
+  }
 
-
-
+  ngOnDestroy() {
+  //   this.sub.unsubscribe()
   }
 
   update(){
     // this.get_all();
+    localStorage.setItem('filter', this.sort);
+    this.get_all();
+  }
+
+  get_all(page = '') {
+    this.sort = localStorage.getItem('filter');
+    this.product = localStorage.getItem('name');
+    this.data = {cate_id: this.cate_id, page: page, perpage:this.count, search: this.product, sort: this.sort };
+    this.http.post<any>('http://192.168.1.155:3000/product/search', this.data).subscribe(result => {
+      this.da = result.data;
+    });
   }
 
   get_count():any {
@@ -77,6 +118,15 @@ export class SaleComponent implements OnInit {
 
   pageChange(newPage: number) {
     // alert(this.router.url);
-		this.router.navigate(['/pages/sale'], { queryParams: { page: newPage } });
+    console.log('after cate_id',this.cate_id);
+    if(this.cate_id != null){
+      // console.log('cate is not null');
+      this.router.navigate(['/pages/sale',this.cate_id], { queryParams: { page: newPage }, skipLocationChange: true });
+      // this.router.navigate(['/pages/sale',this.cate_id]);
+    }else{
+      // console.log('cate is null');
+      this.router.navigate(['/pages/sale'], { queryParams: { page: newPage }, skipLocationChange: true });
+    }
 	}
+
 }
