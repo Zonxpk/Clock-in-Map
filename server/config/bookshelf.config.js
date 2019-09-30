1988841efff4ea86db5367a80233bdefc0b511ff
Zonxgh <pakawat.zon@gmail.com>
2019-09-30 15:46:41 +0700

    Remove server.js (already using RESTful server.)

--- server/config/bookshelf.config.js
+++
@@ -1,8 +0,0 @@
-var knex = require('knex')(require('./db_config'));
-
-var Bookshelf = require('bookshelf')(knex);
-
-// Bookshelf.plugin('registry');
-// Bookshelf.plugin('pagination');
-
-module.exports = Bookshelf;  
