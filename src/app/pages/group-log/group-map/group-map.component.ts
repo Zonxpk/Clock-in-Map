fc1afeb69b30ceecca30cf4fbbc23365fef35f79
Zonxgh <pakawat.zon@gmail.com>
2019-10-10 13:53:19 +0700

    - Improve cluster seperate with condition - Adapt for person-map and group-map

--- src/app/pages/group-log/group-map/group-map.component.ts
+++ src/app/pages/group-log/group-map/group-map.component.ts
@@ -27,6 +27,7 @@ export class GroupMapComponent implements OnInit {
 
   ngOnInit() {
     this.Service_CheckIn.CheckInLog$.subscribe((data) => {
+      data.forEach(element => this.randomDuplicateLatLong(element));
       this.date_markers = data; // And he have data here too!
       this.selectedDate = 0;
       this.get_location(0);
@@ -52,5 +53,80 @@ export class GroupMapComponent implements OnInit {
     const url = 'https://www.google.com/maps?q=loc:' + lat + ',' + long;
     window.open(url);
   }
+
+  randomGeo(center, radius) {
+    const y0 = Number(center.latitude);
+    const x0 = Number(center.longitude);
+    const rd = radius / 111300; // about 111300 meters in one degree
+
+    const u = Math.random();
+    const v = Math.random();
+
+    const w = rd * Math.sqrt(u);
+    const t = 2 * Math.PI * v;
+    const x = w * Math.cos(t);
+    const y = w * Math.sin(t);
+
+    // Adjust the x-coordinate for the shrinking of the east-west distances
+    const xp = x / Math.cos(y0);
+
+    const newlat = y + y0;
+    const newlon = x + x0;
+
+    return {
+        'latitude': newlat.toFixed(5),
+        'longitude': newlon.toFixed(5),
+    };
+  }
+
+  randomDuplicateLatLong(arr) { // verify between lat/long to separate.
+    let duplicated;
+    let lat_arr = [];
+    let long_arr = [];
+    do {
+      duplicated = false;
+      lat_arr = arr.logByDate.map( e => {
+
+        if (e['location']['lc_latitude'] != null) {
+          return Number(e['location']['lc_latitude']).toFixed(5);
+        }else {
+          return Number(13.7894439).toFixed(5);
+        }
+      });
+      long_arr = arr.logByDate.map( e => {
+        if (e['location']['lc_longitude'] != null) {
+          return Number(e['location']['lc_longitude']).toFixed(5);
+        }else {
+          return Number(100.5858895).toFixed(5);
+        }
+      });
+
+      // console.log('long map:' ,long_arr);
+      // console.log('lat map:' ,lat_arr);
+      arr.logByDate.forEach( (element , index) => {
+        if (!element['location']['lc_latitude'] && !element['location']['lc_longitude']) {
+          return;
+        }
+        element['location']['lc_latitude'] = Number(element['location']['lc_latitude']).toFixed(5);
+        element['location']['lc_longitude'] = Number(element['location']['lc_longitude']).toFixed(5);
+        const idx_lat = lat_arr.indexOf(element['location']['lc_latitude']);
+        const idx_long = long_arr.indexOf(element['location']['lc_longitude']);
+        if (idx_lat !== index || idx_long !== index ) {
+          // console.log('(' + index + ') lat/long ' + idx_lat + '/' + idx_long);
+          duplicated = true;
+          const lat = element['location']['lc_latitude'];
+          const long = element['location']['lc_longitude'];
+          const new_value = this.randomGeo({'latitude': lat , 'longitude': long}, 12);
+
+          element['location']['lc_latitude'] = new_value.latitude;
+          element['location']['lc_longitude'] = new_value.longitude;
+        }
+        // console.log('subject: ' + element['ci_subject']);
+        // console.log('lat/long: ' + element['location']['lc_latitude'] + '/' + element['location']['lc_longitude']);
+      });
+    }while (duplicated === true);
+
+  }
+
 
 }
--- src/app/pages/person-log/person-map/person-map.component.ts
+++ src/app/pages/person-log/person-map/person-map.component.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'ngx-person-map',
   templateUrl: './person-map.component.html',
-  styleUrls: ['./person-map.component.scss']
+  styleUrls: ['./person-map.component.scss'],
 })
 export class PersonMapComponent implements OnInit  {
   api: string = environment.apiEndPoint;
@@ -15,8 +15,8 @@ export class PersonMapComponent implements OnInit  {
   lng = 1.581;
   obj = {
     'lat' : 13.7894439,
-    'lng' : 100.5858895
-  }
+    'lng' : 100.5858895,
+  };
   zoom = 18;
   markers: Object;
   constructor(
@@ -47,23 +47,23 @@ export class PersonMapComponent implements OnInit  {
 
 
   randomGeo(center, radius) {
-    var y0 = Number(center.latitude);
-    var x0 = Number(center.longitude);
-    var rd = radius / 111300; //about 111300 meters in one degree
+    const y0 = Number(center.latitude);
+    const x0 = Number(center.longitude);
+    const rd = radius / 111300; // about 111300 meters in one degree
 
-    var u = Math.random();
-    var v = Math.random();
+    const u = Math.random();
+    const v = Math.random();
 
-    var w = rd * Math.sqrt(u);
-    var t = 2 * Math.PI * v;
-    var x = w * Math.cos(t);
-    var y = w * Math.sin(t);
+    const w = rd * Math.sqrt(u);
+    const t = 2 * Math.PI * v;
+    const x = w * Math.cos(t);
+    const y = w * Math.sin(t);
 
-    //Adjust the x-coordinate for the shrinking of the east-west distances
-    var xp = x / Math.cos(y0);
+    // Adjust the x-coordinate for the shrinking of the east-west distances
+    const xp = x / Math.cos(y0);
 
-    var newlat = y + y0;
-    var newlon = x + x0;
+    const newlat = y + y0;
+    const newlon = x + x0;
 
     return {
         'latitude': newlat.toFixed(5),
@@ -75,37 +75,52 @@ export class PersonMapComponent implements OnInit  {
     let duplicated;
     let lat_arr = [];
     let long_arr = [];
-    do{
+    do {
+      // console.log('loop start');
       duplicated = false;
-      lat_arr = arr.map( e => Number(e['location']['lc_latitude']).toFixed(5));
-      long_arr = arr.map( e => Number(e['location']['lc_longitude']).toFixed(5));
+      lat_arr = arr.map( e => {
+        if (e['location']['lc_latitude'] != null) {
+          return Number(e['location']['lc_latitude']).toFixed(5);
+        }else {
+          return Number(13.7894439).toFixed(5);
+        }
+      });
+      long_arr = arr.map( e => {
+        if (e['location']['lc_longitude'] != null) {
+          return Number(e['location']['lc_longitude']).toFixed(5);
+        }else {
+          return Number(100.5858895).toFixed(5);
+        }
+      });
 
+      // console.log('long map:' ,long_arr);
+      // console.log('lat map:' ,lat_arr);
       arr.forEach( (element , index) => {
-        if(!element['location']['lc_latitude'] && !element['location']['lc_longitude']){
+        if (!element['location']['lc_latitude'] && !element['location']['lc_longitude']) {
           return;
         }
         element['location']['lc_latitude'] = Number(element['location']['lc_latitude']).toFixed(5);
         element['location']['lc_longitude'] = Number(element['location']['lc_longitude']).toFixed(5);
-        let idx_lat = lat_arr.indexOf(element['location']['lc_latitude']);
-        let idx_long = long_arr.indexOf(element['location']['lc_longitude']);
-        if (idx_lat !== index || idx_long !== index ){
+        const idx_lat = lat_arr.indexOf(element['location']['lc_latitude']);
+        const idx_long = long_arr.indexOf(element['location']['lc_longitude']);
+        if (idx_lat !== index || idx_long !== index ) {
           // console.log('(' + index + ') lat/long ' + idx_lat + '/' + idx_long);
           duplicated = true;
           const lat = element['location']['lc_latitude'];
           const long = element['location']['lc_longitude'];
-          const new_value = this.randomGeo({'latitude': lat , 'longitude': long}, 15);
+          const new_value = this.randomGeo({'latitude': lat , 'longitude': long}, 12);
 
           element['location']['lc_latitude'] = new_value.latitude;
           element['location']['lc_longitude'] = new_value.longitude;
         }
-        console.log('subject: ' + element['ci_subject']);
-        console.log('lat/long: ' + element['location']['lc_latitude'] + '/' + element['location']['lc_longitude']);
+        // console.log('subject: ' + element['ci_subject']);
+        // console.log('lat/long: ' + element['location']['lc_latitude'] + '/' + element['location']['lc_longitude']);
       });
     }while (duplicated == true);
 
   }
 
-  openLocation(location){
+  openLocation(location) {
     const lat = location['lc_latitude'];
     const long = location['lc_longitude'];
     const url = 'https://www.google.com/maps?q=loc:' + lat + ',' + long;
@@ -114,12 +129,6 @@ export class PersonMapComponent implements OnInit  {
 
 
   clusterClicked(evt) {
-    console.log("clusterClick:", evt);
-
-    // console.log('total markers',evt.getTotalMarkers());
-    // evt.getMarkers().forEach(element => {
-    //   console.log('element',element);
-    // });
   }
 
 }
